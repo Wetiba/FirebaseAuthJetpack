a45ee383480adfb94e6ffad0b0fbdec0851d7091
Wetiba <erickw006@gmail.com>
2023-09-05 11:35:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firebaseauth/navigation/AppNavHost.kt
@@ -0,0 +1,25 @@
+package com.example.firebaseauth.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.firebaseauth.ui.theme.screen.login.LoginScreen
+import com.example.firebaseauth.ui.theme.screen.register.RegisterScreen
+
+@Composable
+fun AppNavHost(modifier: Modifier=Modifier,navController:NavHostController= rememberNavController(),startDestination:String= ROUTE_LOGIN) {
+
+    NavHost(navController = navController, modifier=modifier, startDestination = startDestination ){
+        composable(ROUTE_LOGIN){
+            LoginScreen(navController)
+        }
+        composable(ROUTE_REGISTER){
+            RegisterScreen(navController)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/firebaseauth/navigation/Routes.kt
@@ -0,0 +1,4 @@
+package com.example.firebaseauth.navigation
+
+const val ROUTE_LOGIN="login"
+const val ROUTE_REGISTER="register"
